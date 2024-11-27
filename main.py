6fbd623c01e8524b9353b15de31ec05858be5ede
Ayman Refat <aymanrefatpro@gmail.com>
2024-11-27 15:40:25 +0200

    fix naming issue , create black and white filter

--- main.py
+++ main.py
@@ -33,13 +33,14 @@ def brightness(image: Image, brightness: float) -> Image:
 def darkness(image: Image, darkness: float) -> Image:
     """ Apply a darkness filter to the image
         - darkness: percentage to decrease [0,100%]"""
-    if 0 <= darkness <= 100:
+    if not ( 0 <= darkness <= 100):
         raise ValueError("Darkness must be a positive number")
     return color_filter(image, {"r": -darkness, "g": -darkness, "b": -darkness})
 
 # \text{Luminance} = 0.299 \times R + 0.587 \times G + 0.114 \times B
-def black_and_white(image: Image, use_luminance: bool = True) -> Image:
-    """ Apply a black and white filter to the image """
+def gray(image: Image, use_luminance: bool = True) -> Image:
+    """ Convert the image to grayscale
+        - use_luminance: if True use the luminance formula, otherwise use the average of the RGB values """
     new_image = Image.new("L", (image.width, image.height))
     new_image.load()
     for i in range(image.width):
@@ -59,6 +60,28 @@ def save_image(image: Image, save_path: str) -> None:
     image.save(save_path)
 
 
+def black_and_white(image: Image, threshold: int , use_luminance = True) -> Image:
+    """ Convert the image to black and white
+        - threshold: value to determine if the pixel is black or white """
+    # Convert the image to grayscale
+    if not ( 0 <=threshold <= 256  ) :
+        raise ValueError("Threshold must be between 0 and 256")
+
+    new_image = Image.new("L", (image.width, image.height))
+    new_image.load()
+    for i in range(image.width):
+        for j in range(image.height):
+            r, g, b = image.getpixel((i, j))
+            if use_luminance :
+                luminance = int(0.299 * r + 0.587 * g + 0.114 * b)
+            else :
+                luminance = (r + g + b) // 3
+            if luminance > threshold:
+                new_image.putpixel((i, j), 255)
+            else:
+                new_image.putpixel((i, j), 0)
+    return new_image
+
 def main():
     main_image = load_image("example.jpg")
     image1 = main_image.copy()
@@ -67,19 +90,31 @@ def main():
     image4 = main_image.copy()
     image5 = main_image.copy()
     image6 = main_image.copy()
+    image7 = main_image.copy()
 
     r1 = color_filter(image1, {"r":20, "g":0, "b":0})
     save_image(r1, "output.jpg")
+    print( "Image 1 saved")
     r2 = brightness(image2, 50)
     save_image(r2, "output2.jpg")
+    print( "Image 2 saved")
     r3 = color_filter(image3, {"r":0, "g":30, "b":0})
     save_image(r3, "output3.jpg")
-    r4 = black_and_white(image4)
+    print( "Image 3 saved")
+    r4 = gray(image4)
     save_image(r4, "output4.jpg")
-    r5 = black_and_white(image5, False)
+    print( "Image 4 saved")
+    r5 = gray(image5, False)
     save_image(r5, "output5.jpg")
-    r6 = darkness(image6, 10)
+    print( "Image 5 saved")
+    r6 = darkness(image6, 50)
     save_image(r6, "output6.jpg")
+    print( "Image 6 saved")
+
+    r7 = black_and_white(image7, 128)
+    save_image(r7, "output7.jpg")
+    print( "Image 7 saved")
+
 
 
 if __name__ == "__main__":
